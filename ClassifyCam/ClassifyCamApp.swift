7fdfedd580e2d237f4902f0dae2dbb8c7ffd09cd
rejji <rejji_01@yahoo.com>
2024-10-21 14:06:46 +0530

    Initial Commit

---
+++ ClassifyCam/ClassifyCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassifyCamApp.swift
+//  ClassifyCam
+//
+//  Created by Rajeev Bakshi on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ClassifyCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
